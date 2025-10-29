4f4275d3af4c9cd2f185402376c17bd53b157dac
NeoNeqs <76450252+NeoNeqs@users.noreply.github.com>
2025-10-29 23:03:26 +0100

    Remove english label.

--- sanity.config.ts
+++ sanity.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
                     .title("Content")
                     .items([
                         orderableDocumentListDeskItem({
+                            title: "Portfolio",
                             type: "portfolio",
                             S,
                             context,
