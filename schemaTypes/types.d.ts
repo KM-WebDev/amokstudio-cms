d1a3882b4961bde29624f5040e3e0cce5795df90
NeoNeqs <76450252+NeoNeqs@users.noreply.github.com>
2025-11-19 17:06:53 +0100

    Remove unused types

--- schemaTypes/types.d.ts
+++ schemaTypes/types.d.ts
@@ -16,16 +16,6 @@ import {
 } from "sanity";
 
 import { ColorDefinition } from "@sanity/color-input";
-import { SanityTypeString } from "./types";
-
-export type FieldProps = {
-    title: string;
-    name: string;
-    description?: string;
-    fields?: FieldDefinition[];
-    hidden?: ConditionalProperty;
-    group?: string | string[];
-};
 
 export type ObjectType = Omit<ObjectDefinition & FieldDefinition, "type">;
 export type StringType = Omit<StringDefinition & FieldDefinition, "type">;
@@ -61,56 +51,3 @@ export type Tag = {
     label: string;
     value: string;
 };
-
-export type ImageFieldProps = {
-    initialValue?: InitialValueProperty<any, ImageValue>;
-    validation?: ValidationBuilder<ImageRule, ImageValue>;
-} & FieldProps;
-
-export type SingleLineFieldProps = {
-    validation?: ValidationBuilder<StringRule, string>;
-    initialValue?: InitialValueProperty<any, string>;
-} & FieldProps;
-
-export type MultiLineFieldProps = {
-    validation?: ValidationBuilder<ArrayRule<unknown[]>, unknown[]>;
-} & FieldProps;
-
-export type ObjectArrayFieldProps = {
-    elementType: SanityTypeString | string;
-} & FieldProps;
-
-export type DocumentFieldProps = {
-    groups?: FieldGroupDefinition[];
-} & FieldProps;
-
-export type DropDownFieldProps = {
-    options: string[];
-    initialValue: InitialValueProperty<any, string>;
-    readOnly?: boolean;
-} & FieldProps;
-
-export type TagFielsProps = {
-    tags: Tag[];
-} & FieldProps;
-
-export type SanityTypeString =
-    | "string"
-    | "number"
-    | "boolean"
-    | "object"
-    | "array"
-    | "block"
-    | "date"
-    | "datetime"
-    | "document"
-    | "file"
-    | "geopoint"
-    | "image"
-    | "reference"
-    | "crossDatasetReference"
-    | "globalDocumentReference"
-    | "slug"
-    | "text"
-    | "url"
-    | "email";
