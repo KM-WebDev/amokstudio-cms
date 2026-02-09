cb622b4231a68c722ec87dfeb3e54ebf79c32bb4
NeoNeqs <76450252+NeoNeqs@users.noreply.github.com>
2026-02-09 20:52:53 +0100

    Add og description.

--- schemaTypes/documents/process/ProcessTitle.ts
+++ schemaTypes/documents/process/ProcessTitle.ts
@@ -18,5 +18,9 @@ export const ProcessTitle = defineSingletonDocument({
             name: "description",
             title: "Opis Strony",
         }),
+        defineLocalizedText({
+            name: "ogTitle",
+            title: "Tytuł udostępniania w mediach społecznościowych",
+        }),
     ],
 });
